0c904dcaa94294c0837f40ac4e29286ce141bebe
monorifanni <m.fanni01@gmail.com>
2022-11-18 10:45:45 +0100

    create navbar, banner, add css

---
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', function () {
+      const navbar = document.querySelector('.navbar');
+      if(navbar){
+        navbar.classList.toggle('sticky', window.scrollY > 0);
+      }
+    })
+  }
+
+  toggle() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    if (menuToggle && navigation) {
+      menuToggle.classList.toggle('active');
+      navigation.classList.toggle('active');
+    }
+  }
+}
